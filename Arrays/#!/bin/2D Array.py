b7060c464a7216e590521de03b8d90709904025b
Kavish Nag <kavishnag13@gmail.com>
2026-01-19 23:24:34 +0530

    Improve loop structure in hourglassSum function
    Refactor nested loops for better readability.

---
+++ Arrays/#!/bin/2D Array.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+def hourglassSum(arr):
+    max_sum = -math.inf  # handles negative values properly
+    for i in range(4):       
+        for j in range(4):    
+            current_sum = (
+                arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+                arr[i+1][j+1] +
+                arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            )
+            max_sum = max(max_sum, current_sum)
+    return max_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = hourglassSum(arr)
+    fptr.write(str(result) + '\n')
+    fptr.close()
